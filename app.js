e5d7dfa9ba53f7262a0b8565eb30ddae116a47ea
Stephen <stolak>
2023-08-02 15:31:00 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ app.get('/names', (req, res) => {
   res.send('Akinbobola Olawolettdt');
 })
 
+app.get('/tutorial', (req, res) => {
+  res.send('Samson and github');
+})
 app.listen(6000, () => {
   console.log("Server is running on port 6000");
 })
